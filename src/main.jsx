82d5891587beb111796e8340ceac8eced130f434
Aravindhan G.K <aravindhang.k2004@gmail.com>
2026-09-17 14:53:43 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import FuelGoApp from "./FuelGoApp.jsx";
 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <FuelGoApp />
+  </React.StrictMode>
+);
